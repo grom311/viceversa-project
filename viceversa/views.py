b4f57d11fb926e7e6c46116cd6bff3af8573212f
grom311 <grom311@yandex.ru>
2021-01-17 17:44:55 +0300

    default page

--- viceversa/views.py
+++ viceversa/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reverse_txt = user_text[::-1]
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reverse_txt})
+
